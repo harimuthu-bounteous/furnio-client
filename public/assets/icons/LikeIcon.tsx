760a2927b8c82456b4ca7ab172164c75843f48c2
Hari Muthu Muthupandi <harimuthu.muthupandi@bounteous.com>
2024-10-04 11:37:01 +0530

    add: images, icons

---
+++ public/assets/icons/LikeIcon.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+
+interface LikeIconProps {
+  className?: string;
+}
+
+const LikeIcon: FC<LikeIconProps> = ({ className }) => {
+  return (
+    <svg
+      width="16"
+      height="15"
+      viewBox="0 0 16 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M7.99973 13.0361C-5.33333 5.66667 3.99999 -2.33333 7.99973 2.72537C12 -2.33334 21.3333 5.66667 7.99973 13.0361Z"
+        stroke="white"
+        strokeWidth="1.8"
+      />
+    </svg>
+  );
+};
+
+export default LikeIcon;
